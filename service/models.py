e761e5032ac497ecbe8eaf1d5db2b30ee253b2dc
Taras Nester <tarasnester414@gmail.com>
2022-07-29 18:59:03 +0300

    added check task subscribers logic

--- service/models.py
+++ service/models.py
@@ -2,8 +2,11 @@ import os
 import uuid
 
 from django.db import models
+from django.db.models.signals import pre_save, m2m_changed, post_save
+from django.dispatch import receiver
 
 from todo import settings
+from service.tasks import add, send_email
 
 
 class Task(models.Model):
@@ -27,6 +30,41 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+
+temporary_subscribers = None
+
+
+@receiver(post_save, sender=Task)
+def clear_tempory_user_variable(sender, instance, **kwargs):
+    global temporary_subscribers
+    print("post save", temporary_subscribers)
+    temporary_subscribers = None
+
+
+@receiver(m2m_changed, sender=Task.subscribers.through)
+def send_email_if_task_change_subscribers(sender, instance, **kwargs):
+    global temporary_subscribers
+    new_subscribers = set(instance.subscribers.all())
+    old_subscribers = temporary_subscribers
+    if old_subscribers is not None and old_subscribers != new_subscribers:
+        added_subscribers = new_subscribers.difference(old_subscribers)
+        deleted_subscribers = old_subscribers.difference(new_subscribers)
+        for subscriber in added_subscribers:
+            send_email.delay(
+                receiver_name=subscriber.username,
+                receiver_email=subscriber.email,
+                task=instance.title
+            )
+        for subscriber in deleted_subscribers:
+            send_email.delay(
+                receiver_name=subscriber.username,
+                receiver_email=subscriber.email,
+                task=instance.title,
+                subscribed=False
+            )
+    else:
+        temporary_subscribers = new_subscribers
 
 
 def task_image_file_path(instance, filename):
--- service/tasks.py
+++ service/tasks.py
@@ -1,9 +1,24 @@
 from __future__ import absolute_import, unicode_literals
 
+import time
 
 from celery import shared_task
 
 
 @shared_task()
 def add(x, y):
-    return x + y
+    print("async=", x + y)
+    return
+
+
+@shared_task()
+def send_email(receiver_name: str, receiver_email: str, task: str, subscribed: bool = True):
+    action = "subscribed to the task"
+    if not subscribed:
+        action = "unsubscribed from the task"
+
+    print(f"email to {receiver_name}")
+    print(f"adress {receiver_email}")
+    print(f"you have been  {action} to task {task}")
+    print("-" * 20)
+    time.sleep(5)
